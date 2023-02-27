5a478b163f9477f11a62f9236dae993b3455da0a
nprati01 <nicole.e.prati@gmail.com>
2023-02-27 15:10:58 -0500

    file will do

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+  /*----- constants -----*/
+//   1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1.
+const colors = null
+
+//   1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value.
+
+
+
+  /*----- state variables -----*/
+//   2.1) Use a board array to represent the squares.
+let boardArray[]
+//   2.2) Use a turn variable to remember whose turn it is.
+let playerOneTurn
+let playerTwoTurn
+//   2.3) Use a winner variable to represent three different possibilities - player that won, a tie, or game in play.
+
+let gameWinner
+let gameTie
+let gamePlay
+
+
+  /*----- cached elements  -----*/
+  //3.1) Store the 9 elements that represent the squares on the page.
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
